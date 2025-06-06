4887c581bf968bd964efd066f4e77c6e17dd5611
alwin-04 <2322ja05@kgcas.com>
2025-06-06 10:46:33 +0530

    Create DemoMethod.java

---
+++ DemoMethod.java
@@ -0,0 +1,12 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class DemoMethod {
+			
+	public static void detials(String name,String ambition,String desigination)
+	{
+	     System.out.println("I am"+" "+name+""+"My desigination"+desigination+""+"And My ambiton is"+ambition);	
+	}
+
+}
