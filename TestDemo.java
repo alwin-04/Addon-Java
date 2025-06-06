7dcf80a28b93fe10dd31e0c8614212d3e9efd78d
alwin-04 <2322ja05@kgcas.com>
2025-06-06 10:47:17 +0530

    Create TestDemo.java

---
+++ TestDemo.java
@@ -0,0 +1,11 @@
+package Day2;
+
+import java.util.Scanner;
+
+public class TestDemo {
+	public static void main(String[] args) {
+		DemoMethod.detials("alwin", "Developer", "Place in a good company");
+		
+	}
+
+}
