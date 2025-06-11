3016183ef976847162b53f0d6db53d47c8c26e6c
alwin-04 <2322ja05@kgcas.com>
2025-06-11 09:37:04 +0530

    Create TestSBI_Account.java

---
+++ day4/TestSBI_Account.java
@@ -0,0 +1,11 @@
+package Day3;
+
+public class TestSBI_Account {
+	public static void main(String[] args) {
+		SBI_Account sbi=new SBI_Account("Alwin",100000); 
+		sbi.setAccHolder("Alwin");
+		sbi.deposite(2000);
+		
+	}
+
+}
