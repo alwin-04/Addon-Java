889b51445d4999795c908dfbf145bae797eac1b6
alwin-04 <2322ja05@kgcas.com>
2025-06-12 12:55:12 +0530

    Create .HashSet.java

---
+++ day6/.HashSet.java
@@ -0,0 +1,15 @@
+package Day6;
+
+import java.util.HashSet;
+
+public class Email {
+	public static void main(String[] args) {
+	HashSet<String> email= new HashSet<>();
+	email.add("alwin777@gmail.com");
+	email.add("bino793@gmail.com");
+	for(String i:email)
+	{
+		System.out.println(i);
+	}
+	}
+}
